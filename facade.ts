a7d57180044cbf84f701c8da77837109ae8c0cd1
Esteban-Reyes-Anguiano <139920025+Esteban-Reyes-Anguiano@users.noreply.github.com>
2023-08-03 20:44:06 -0600

    Iteration 3: Implement a Facade

--- facade.ts
+++ facade.ts
@@ -1,7 +1,30 @@
-import { SubsystemA, SubsystemB, SubsystemC } from './subsystems';
+import  SubsystemA   from './subsystems/subSystemA';
+import  SubsystemB  from './subsystems/subSystemB';
+import  SubsystemC  from './subsystems/subSystemC';
 
 class Facade {
-  // TODO: Implement the Facade class using the Facade pattern
+  private subsystemA: SubsystemA;
+  private subsystemB: SubsystemB;
+  private subsystemC: SubsystemC;
+
+  constructor() {
+    this.subsystemA = new SubsystemA();
+    this.subsystemB = new SubsystemB();
+    this.subsystemC = new SubsystemC();
+  }
+
+  // Provide a simple interface to clients that hides the complexity of the subsystems.
+  public operationA(): string {
+    return `${this.subsystemA.operationA1()} ${this.subsystemA.operationA2()}`;
+  }
+
+  public operationB(): string {
+    return `${this.subsystemB.operationB1()} ${this.subsystemB.operationB2()}`;
+  }
+
+  public operationC(): string {
+    return `${this.subsystemC.operationC1()} ${this.subsystemC.operationC2()}`;
+  }
 }
 
 export default Facade;
